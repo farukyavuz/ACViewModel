4cbeee6cc523824f945b5a44e4db40deb0b0b83b
Faruk Yavuz <fyavuz@hurriyet.com.tr>
2018-03-30 17:43:14 +0300

    Added diffutil.

---
+++ app/src/main/java/org/faruk/acviewmodel/home/RepoDiffCallback.java
@@ -0,0 +1,43 @@
+package org.faruk.acviewmodel.home;
+
+import android.support.v7.util.DiffUtil;
+
+import org.faruk.acviewmodel.model.Repo;
+
+import java.util.List;
+
+/**
+ * Created by farukyavuz on 30.03.2018.
+ * Copyright (c) 2018 Hürriyet to present
+ * All rights reserved.
+ */
+public class RepoDiffCallback extends DiffUtil.Callback {
+
+    private final List<Repo> oldList;
+    private final List<Repo> newList;
+
+    RepoDiffCallback(List<Repo> oldList, List<Repo> newList) {
+        this.oldList = oldList;
+        this.newList = newList;
+    }
+
+    @Override
+    public int getOldListSize() {
+        return oldList.size();
+    }
+
+    @Override
+    public int getNewListSize() {
+        return newList.size();
+    }
+
+    @Override
+    public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).id() == newList.get(newItemPosition).id();
+    }
+
+    @Override
+    public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+        return oldList.get(oldItemPosition).equals(newList.get(newItemPosition));
+    }
+}
